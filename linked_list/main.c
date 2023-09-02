38ac0a05f269573ea90f90f8ceaafb8283669cc9
Eddy <eduardosanchezsimancas@gmail.com>
2023-09-02 16:45:52 -0700

    fixed bugs
    fixed bug in print and count function. I was used the list to traverse itself but this caused the pointer to lose the location of the head node and thus lost the entire list. I used a temp variable to fix this issue

--- linked_list/main.c
+++ linked_list/main.c
@@ -1,34 +1,54 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "linked_list.h"
 
-typedef struct node {
-    int value;
-    struct node *next;
-} node_t;
-
-void numpp(int copy);
-void p_numpp(int *copy);
 
 int main() {
-    int num = 1;
+    char *menu = 
+    "0) quit\n"
+    "1) create\n"
+    "2) destory\n"
+    "3) print\n"
+    "4) insert at\n"
+    ">> ";
+    int reply = 0;
+    _Bool flag = 1;
+    linked_list_t *users_list = NULL;
+    // users_list->list_head = NULL; users_list->list_tail = NULL;
 
-    printf("num = %d\n", num);
-    numpp(num);
-    printf("num = %d\n", num);
-    p_numpp(&num);
-    printf("num = %d\n", num);
+    while (flag) {
+        printf("%s", menu);
+        scanf("%d", &reply);
+
+        switch (reply) {
+            case 0:
+                flag = 0;
+                break;
+            case 1:
+                printf("enter value: ");
+                scanf("%d", &reply);
+                linked_list_create(&users_list, reply);
+                break;
+            case 2:
+                linked_list_destroy(&users_list);
+                break;
+            case 3:
+                linked_list_print(users_list);
+                break;
+            case 4:
+                printf("enter index: ");
+                uint64_t index = 0;
+                scanf("%lu", &index);
+                printf("enter value: ");
+                scanf("%d", &reply);
+                linked_list_insert_at(&users_list, index, reply);
+                break;
+            default:
+                printf("invalid input\n\n");
+                break;
+        }
+    }
+    
    
     return 0;
-}
-
-void numpp(int copy) {
-    copy += 1;
-
-    printf("copy = %d\n", copy);
-}
-
-void p_numpp(int *copy) {
-    (*copy) += 1;
-
-    printf("*copy = %d\n", *copy);
 }
