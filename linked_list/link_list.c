286245168c309aa437c02e8d6130a0c301650079
Eddy <eduardosanchezsimancas@gmail.com>
2023-09-02 10:17:42 -0700

    version 1.1
    made some progress on a few functions. No testing has been made yet

--- linked_list/link_list.c
+++ linked_list/link_list.c
@@ -1,49 +1,152 @@
 #include "link_list.h"
-#include <stdbool.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-int8_t linked_list_create(linked_list_t **head, int value) {
+
+int8_t linked_list_create(linked_list_t **list, int value) {
+    /**
+     * @brief  initialized a linked list object with 'value' stored in the first node
+     * 
+     * @param  list     the linked list object
+     * @param  value    the value to store in the first node
+     * @return int8_t   error code; -1 = fatel error, 0 = no error, 1 = user error
+    **/
     // check if list already exist
-    if ( (*head)->list_head != NULL ){
+    if ( (*list)->list_head != NULL ){
         return 1;
     }
 
     // create
-    if ((*head)->list_head = 
-        (linked_list_t *) malloc(sizeof(linked_list_t))
+    if (
+        ( (*list)->list_head = (linked_list_t *) malloc(sizeof(linked_list_t)) )
         == NULL
     ) {
         return -1;
     }
 
     // init
-    (*head)->list_tail = NULL;
-    (*head)->list_head->item = value;
+    (*list)->list_tail = NULL;
+    (*list)->list_head->item = value;
 
     return 0;
 }
 
 
-int8_t linked_list_destory(linked_list_t **head) {
+int8_t linked_list_destory(linked_list_t **list) {
+    /**
+     * @brief  frees all the memory allocated to a linked list
+     * 
+     * @param  list     the linked list to free
+     * @return int8_t   error code; -1 = fatel error, 0 = no error, 1 = user error
+    **/
     // check if list already exist
-    if ( (*head)->list_head != NULL ){
+    if ( (*list)->list_head != NULL ){
         return 1;
     }
 
     // traverse list
-    linked_list_t *temp = NULL;
+    node_t *temp = NULL;
 
-    while ( (*head)->list_head != NULL ) {
-        temp = (*head)->list_head->next_node;
-        free( (*head)->list_head );
-        (*head)->list_head = temp;
+    while ( (*list)->list_head != NULL ) {
+        temp = (*list)->list_head->next_node;
+        free( (*list)->list_head );
+        (*list)->list_head = temp;
     }
 
     // danglers begone
-    (*head)->list_head = NULL;
-    (*head)->list_tail = NULL;
+    (*list)->list_head = NULL;
+    (*list)->list_tail = NULL;
     temp = NULL;
 
     return 0;
+}
 
+
+uint64_t linked_list_size(linked_list_t *list) {
+    /**
+     * @brief  returns the total number of nodes in linked list
+     * 
+     * @param  list         the linked list object
+     * @return uint64_t     the total number of nodes
+    **/
+    uint_fast64_t counter = 0;
+
+    // traverse linked list
+    while ( list->list_head != NULL ) {
+        counter++;
+        list->list_head = list->list_head->next_node;
+    }
+
+    return counter;
+}
+
+
+int8_t linked_list_insert_at(linked_list_t **list, uint64_t index, int value) {
+    /**
+     * @brief  inserts node at index given by user
+     * 
+     * @param  list     linked list
+     * @param  index    index to insert
+     * @return int8_t   error code
+    **/
+    if ( (*list)->list_head == NULL ) {
+        // invalid list
+        return 1;
+    }
+
+    if ( index > linked_list_size(*list) + 1 ) {
+        // invalid index
+        return 1;
+    }
+
+    // create node
+    node_t *new_node = NULL;
+    if (
+        ( new_node = (node_t *) malloc(sizeof(node_t)) )
+        == NULL
+    ) {
+        return -1;
+    }
+    new_node->item = value;
+
+    // insert at head
+    if ( index == 0 ) {
+        new_node->next_node = (*list)->list_head;
+        (*list)->list_head = new_node;        
+        return 0;
+    }
+
+    // insert at tail
+    if ( index == linked_list_size(*list) + 1 ) {
+        (*list)->list_tail = new_node;
+        new_node->next_node = NULL;
+        return 0;
+    }
+
+    // insert inbetween list
+    node_t *current_node = (*list)->list_head;
+
+    while ( index != 1 ) {
+        current_node = current_node->next_node;
+        index--;
+    }
+
+    new_node->next_node = current_node->next_node;
+    current_node->next_node = new_node;
+
+    current_node = NULL;
+
+    return 0;
+}
+
+
+int8_t linked_list_print(linked_list_t *list) {
+    fprintf(stdout, "linked list: ");
+    // traverse linked list
+    while ( list->list_head != NULL ) {
+        fprintf(stdout, "%d ", list->list_head->item);
+        list->list_head = list->list_head->next_node;
+    }
+    
+    return 0;
 }
--- linked_list/link_list.h
+++ linked_list/link_list.h
@@ -6,7 +6,7 @@
  *  0  = no error
  *  1  = user error
  *  -1 = fatel error 
- * @version 1.0
+ * @version 1.1
  * @date 2023-09-01
  * 
  * @copyright Copyright (c) 2023
@@ -20,9 +20,11 @@
 #include <stdint.h>
 
 /*** structs ***/
+typedef struct node node_t;
+
 typedef struct node {
     int item;
-    struct node *next_node;
+    node_t *next_node;
 }node_t;
 
 typedef struct linked_list {
@@ -32,15 +34,16 @@ typedef struct linked_list {
 
 
 /*** function declaration ***/
-int8_t linked_list_create(linked_list_t **head, int value);
-int8_t linked_list_destory(linked_list_t **head);
-// int linked_list_insert_at();
+int8_t linked_list_create(linked_list_t **list, int value);
+int8_t linked_list_destory(linked_list_t **list);
+uint64_t linked_list_size(linked_list_t *list);
+int8_t linked_list_insert_at(linked_list_t **list, uint64_t index, int value);
 // int linked_list_push();
 // int linked_list_append();
 // int linked_list_delete_at();
 // int linked_list_remove();
 // int linked_list_reverse();
-// int linked_list_print();
+int8_t linked_list_print(linked_list_t *list);
 
 
 #endif //_LINK_LIST_H_
